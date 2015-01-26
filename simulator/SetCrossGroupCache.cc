2c82b2efaa17d85fd378549117af7eb383d0e76e
刘当一 <LeedyPKU@gmail.com>
2015-01-26 20:17:46 +0800

    fix for last commit

---
+++ simulator/SetCrossGroupCache.cc
@@ -0,0 +1,248 @@
+#include "SetCrossGroupCache.hh"
+#include "strings.h"
+
+inline uint64_t getTagBits(Addr addr)
+{
+    return (addr & TAG_MASK) >> __builtin_ctz(TAG_MASK);
+}
+
+inline uint64_t getStripBits(Addr addr)
+{
+    return (addr & STRIP_MASK) >> __builtin_ctz(STRIP_MASK);
+}
+
+inline uint64_t getLineBits(Addr addr)
+{
+    return (addr & LINE_MASK) >> __builtin_ctz(LINE_MASK);
+}
+
+inline uint64_t getOffsetBits(Addr addr)
+{
+    return (addr & OFFSET_MASK) >> __builtin_ctz(OFFSET_MASK);
+}
+
+void SetCrossGroupCache::nextState(Tick tick)
+{
+    switch (state) {
+    case StateIdle:
+        break;
+    case StateMoving: {
+        targetRequest.shiftTime++;
+        if (targetOp == OpRead) {
+            HeadDirection dir = strip[targetStrip]->readDir(targetAddr);
+            strip[targetStrip]->shift(dir);
+            if (!strip[targetStrip]->readDir(targetAddr)) {
+                changeState(StateReading, tick);
+            }
+        }
+        else {
+            HeadDirection dir = strip[targetStrip]->writeDir(targetAddr);
+            strip[targetStrip]->shift(dir);
+            if (!strip[targetStrip]->writeDir(targetAddr)) {
+                changeState(StateWriting, tick);
+            }
+        }
+        break;
+    }
+    case StateReading: {
+        strip[targetStrip]->readDir(targetAddr);
+        smu[targetStrip][targetGroup][targetGroupOffset].lastAccessTick = tick;
+        callback(targetRequest);
+        changeState(StateReturning, tick);
+        break;
+    }
+    case StateWriting: {
+        strip[targetStrip]->writeDir(targetAddr);
+        smu[targetStrip][targetGroup][targetGroupOffset].lastAccessTick = tick;
+        SMUEntry newEntry = { tick, targetTag, true };
+        smu[targetStrip][targetGroup][targetGroupOffset] = newEntry;
+        changeState(StateReturning, tick);
+        break;
+    }
+    case StateLookup: {
+        SMUEntry hitEntry;
+        int groupOffset;
+        hitEntry.valid = false;
+        for (int i = 0; i < GROUPSIZE; i++) {
+            SMUEntry entry = smu[targetStrip][targetGroup][i];
+            if (entry.valid && targetTag == entry.tag) {
+                hitEntry = entry;
+                groupOffset = i;
+                break;
+            }
+        }
+        if (hitEntry.valid) {
+            targetGroupOffset = groupOffset;
+            targetAddr = (getLineBits(target) << GROUP_BIT) + groupOffset;
+            if (targetOp == OpRead) {
+                if (!strip[targetStrip]->readDir(targetAddr)) {
+                    changeState(StateReading, tick);
+                }
+                else {
+                    changeState(StateMoving, tick);
+                }
+            }
+            else {
+                callback(targetRequest);
+                if (!strip[targetStrip]->writeDir(targetAddr)) {
+                    changeState(StateWriting, tick);
+                }
+                else {
+                    changeState(StateMoving, tick);
+                }
+            }
+        }
+        else {
+            if (targetOp != OpRead) {
+                callback(targetRequest);
+            }
+            int groupOffset;
+            Tick oldestTick = 0;
+            for (int i = 0; i < GROUPSIZE; i++) {
+                SMUEntry entry = smu[targetStrip][targetGroup][i];
+                if (!entry.valid || entry.lastAccessTick < oldestTick) {
+                    groupOffset = i;
+                }
+            }
+            targetGroupOffset = groupOffset;
+            targetAddr = (getLineBits(target) << GROUP_BIT) + targetGroupOffset;
+            missCounter = missPenalty;
+            changeState(StateMiss, tick);
+        }
+        break;
+    }
+    case StateMiss: {
+        targetRequest.isMissed = true;
+        if (missCounter) {
+            missCounter--;
+            HeadDirection dir = strip[targetStrip]->writeDir(targetAddr);
+            strip[targetStrip]->shift(dir);
+            changeState(StateMiss, tick);
+        }
+        else {
+            if (targetOp == OpRead) {
+                callback(targetRequest);
+            }
+            targetOp = OpWrite;
+            if (!strip[targetStrip]->writeDir(targetAddr)) {
+                changeState(StateWriting, tick);
+            }
+            else {
+                changeState(StateMoving, tick);
+            }
+        }
+        break;
+    }
+    case StateReturning: {
+        int count = 0;
+        int clean = true;
+        for (int i = 0; i < (1 << STRIP_BIT); i++) {
+            if (strip[i]->offset != 0) {
+                strip[i]->shift(strip[i]->toDir(0));
+                count++;
+                if (strip[i]->offset != 0) {
+                    clean = false;
+                }
+                if (count == 2) {
+                    break;
+                }
+            }
+        }
+        if (clean) {
+            changeState(StateIdle, tick);
+        }
+        break;
+    }
+    }
+}
+
+int64_t SetCrossGroupCache::stateLength(State state)
+{
+    switch (state) {
+    case StateIdle:
+        return 1;
+    case StateMoving:
+        return 1;
+    case StateReading:
+        return 1;
+    case StateLookup:
+        return 0;
+    case StateMiss:
+        return 1;
+    case StateWriting:
+        return 1;
+    case StateReturning:
+        return 1;
+    }
+}
+
+void SetCrossGroupCache::nextTick(Tick tick)
+{
+    for (int64_t currentMicroTickLeft = microTickPerTick; currentMicroTickLeft >= stateLength(state);) {
+        currentMicroTickLeft -= stateLength(state);
+        nextState(tick);
+    }
+}
+
+void SetCrossGroupCache::requestCache(Request request)
+{
+    if (state != StateIdle and state != StateReturning) {
+        cacheDesignError("new request come while cache is still working!!!");
+    }
+    else {
+        targetOp = request.op;
+        targetRequest = request;
+        target = request.address;
+        targetGroup = getLineBits(target);
+        targetStrip = getStripBits(target);
+        targetTag = getTagBits(target);
+        state = StateLookup;
+    }
+}
+
+bool SetCrossGroupCache::isAvailable()
+{
+    return (state == StateIdle or state == StateReturning);
+}
+
+std::string SetCrossGroupCache::stateToString(State _state)
+{
+    switch (_state) {
+    case StateIdle:
+        return "StateIdle";
+        break;
+    case StateMoving:
+        return "StateMoving";
+        break;
+    case StateReading:
+        return "StateReading";
+        break;
+    case StateWriting:
+        return "StateWriting";
+        break;
+    case StateLookup:
+        return "StateLookup";
+        break;
+    case StateMiss:
+        return "StateMiss";
+        break;
+    case StateReturning:
+        return "StateReturning";
+        break;
+    }
+}
+
+void SetCrossGroupCache::changeState(State _state, Tick tick)
+{
+    if (state != _state) {
+        char text[1000];
+        sprintf(text, "@%lld:State change from %s to %s", tick, stateToString(state).c_str(), stateToString(_state).c_str());
+        state = _state;
+        cacheDesignNotification(text);
+    }
+}
+
+std::string SetCrossGroupCache::getStateName()
+{
+    return stateToString(state);
+}
---
+++ simulator/SetCrossGroupCache.hh
@@ -0,0 +1,76 @@
+#ifndef __SET_CROSS_GROUP_CACHE_HH__
+#define __SET_CROSS_GROUP_CACHE_HH__
+
+#include "BaseCache.hh"
+#include "CacheStrip.hh"
+
+#define OFFSET_BIT 6
+#define OFFSET_MASK 0x3F
+#define LINE_BIT 3
+#define LINE_MASK 0x70000
+#define STRIP_BIT 10
+#define STRIP_MASK 0xFFC0
+#define TAG_BIT 13
+#define TAG_MASK 0xFFF80000
+#define GROUP_BIT 3
+#define GROUPSIZE 8
+
+class SetCrossGroupCache : public BaseCache {
+    struct SMUEntry {
+        Tick lastAccessTick;
+        uint64_t tag;
+        bool valid;
+    };
+private:
+    typedef enum : uint8_t {
+        StateIdle,
+        StateMoving,
+        StateReading,
+        StateLookup,
+        StateMiss,
+        StateWriting,
+        StateReturning,
+    } State;
+    State state;
+    Request targetRequest;
+    uint64_t target; // Target address passed by request
+    
+    //Target = targetTag:TargetStrip:TargetGroup:offset
+    uint64_t targetGroup;
+    uint64_t targetStrip;
+    uint64_t targetTag;
+    
+    //TargetAddr = targetGroup:targetGroupOffset
+    uint64_t targetAddr;
+    uint64_t targetGroupOffset;
+    
+    uint32_t missCounter;
+    Operation targetOp;
+    CacheStrip * strip[1<<STRIP_BIT];
+    SMUEntry smu[1<<STRIP_BIT][1<<LINE_BIT][GROUPSIZE];
+    void changeState(State, Tick);
+    std::string stateToString(State);
+    
+    static const int64_t microTickPerTick = 1;
+    void nextState(Tick);
+    int64_t stateLength(State);
+public:
+    SetCrossGroupCache(callback_type callback):BaseCache(callback){
+        for (int i=0 ; i<(1<<STRIP_BIT); i++) {
+            strip[i]= new CacheStrip({2,6,10,14,18,22,26,30,34,38,42,46,50,54,58,62}, {2,62});
+        }
+        for (int i=0; i<(1<<STRIP_BIT); i++) {
+            for (int j=0; j<(1<<LINE_BIT); j++) {
+                for (int k=0; k<GROUPSIZE; k++) {
+                    smu[i][j][k].valid = false;
+                }
+            }
+        }
+    }
+    virtual void nextTick(Tick tick);
+    virtual void requestCache(Request);
+    virtual bool isAvailable();
+    virtual std::string getStateName();
+};
+
+#endif
